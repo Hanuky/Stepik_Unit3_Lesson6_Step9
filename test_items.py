bad83b1b668ae544d44cecc762fdf138f4650273
Andrey Gorbanev <gorbanevaa@hotmail.com>
2021-05-04 10:04:57 +0300

    fix assertion error and add error handler for NoSuchElementException

--- test_items.py
+++ test_items.py
@@ -1,9 +1,17 @@
 import pytest
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 
-def test_chek_see_basket_button(browser):
-    link = " http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-    browser.get(link)
-    chek_button = browser.find_element_by_css_selector("button.btn.btn-add-to-basket")
+class TestProductPage():
+    def element_exist(self, selector, browser):
+        try:
+            x = browser.find_element_by_css_selector(selector)
+            return True
+        except NoSuchElementException:
+            return False    
 
-    assert chek_button, "Button not found!!!"
+    def test_chek_see_basket_button(self, browser):
+        link = " http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+        browser.get(link)
+        chek_button = self.element_exist("button.btn.btn-add-to-basket", browser)
+        assert chek_button, "Button not found!!!"
